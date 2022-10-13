b0fc9c4258f98c57fe8edebdb04b716eb5372367
Rodrigo Maldonado <rmaldonado@iptel.com.ar>
2022-10-13 12:23:34 -0300

    routes 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { Layout1Component } from './pages/layout1/layout1.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'index', component:IndexComponent},
+  {path:'layout1', component:Layout1Component},
+  {path:'', component:IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
