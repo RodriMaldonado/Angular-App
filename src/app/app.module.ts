acd10b76bd46389de1b870a0e8ab68bdadd38a31
Rodrigo Maldonado <rmaldonado@iptel.com.ar>
2022-10-24 11:42:34 -0300

    ngx error solucionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
-import {NgxSoapModule} from 'ngx-soap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './pages/index/index.component';
