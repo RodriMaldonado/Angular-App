d3e6774f14b1e565b1b6fe9f002bc722c4f1a720
Rodrigo Maldonado <rmaldonado@iptel.com.ar>
2022-10-31 11:21:39 -0300

    validacion de inputs lista

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,28 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/users/user.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Login } from 'src/app/model/login';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent{
-  email!:string;
-  password!:string;
+export class IndexComponent implements OnInit{
+  form:FormGroup;
 
-  constructor(public userService: UsersService,public router: Router) { 
-    
+  constructor(private formBuilder:FormBuilder, private userService: UsersService,private router: Router) { 
+    this.form=this.formBuilder.group({
+      email:["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    })
+   }
+
+   get email()
+   {
+    return this.form.get("email");
+   }
+
+   get password()
+   {
+    return this.form.get("password")
+   }
+
+  ngOnInit(): void {
   }
 
-  login()  {
-    const user = {email: this.email, password: this.password};
-    this.userService.login(user).subscribe(data => {
+  
+
+  login()  
+  {
+    if(this.form.valid)
+    {
+      let email:string=this.form.get('email')?.value;
+      let password:string=this.form.get('password')?.value;
+
+      let login:Login= new Login(email,password)
+
+    this.userService.login(login).subscribe(data => {
         //preguntar si tenemos el token para avanzar a dejar en una cookie de lo contrario mostrar mensaje de error en el usurio
         this.userService.setToken(data.token);
         });
     
     this.router.navigateByUrl('layout1');
+      }
     
 }
 }
