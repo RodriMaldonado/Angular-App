0248f1edc83a572ab7099119e7b649a5240d5976
Rodrigo Maldonado <rmaldonado@iptel.com.ar>
2022-10-31 10:37:54 -0300

    merge listo

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,65 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/users/user.service';
-<<<<<<< HEAD
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Login } from 'src/app/model/login';
-=======
-import { Router } from '@angular/router';
->>>>>>> nelson_api
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent implements OnInit{
+export class IndexComponent{
+  email!:string;
+  password!:string;
 
-<<<<<<< HEAD
-  form:FormGroup;
-  
-
-  constructor(public userService: UsersService, private formBuilder:FormBuilder, private router:Router) { 
-    this.form=this.formBuilder.group({
-      email:["", [Validators.required, Validators.email]],
-      password: ["", [Validators.required,Validators.maxLength(8),Validators.minLength(5) ]]
-    })
-  }
-
-  get email()
-  {
-    return this.form.get("email");
-  }
-
-  get password()
-  {
-    return this.form.get("password")
-  }
-
-  ngOnInit(): void {
-    
-  }
-
-  formLogin() {
-    if(this.form.valid)
-    {
-      let email:string=this.form.get('email')?.value;
-      let password:string=this.form.get('password')?.value;
-
-      let login:Login= new Login(email,password)
-
-    this.userService.formLogin(login).subscribe( data => {
-      console.log(data)
-      this.router.navigate(['layout1']);
-
-  },
-  respuestaError=>{
-    alert("Se ha producido un error")
-  })
-
-  
-
-=======
   constructor(public userService: UsersService,public router: Router) { 
     
   }
@@ -73,7 +24,5 @@ export class IndexComponent implements OnInit{
     
     this.router.navigateByUrl('layout1');
     
->>>>>>> nelson_api
-}
 }
 }
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders} from "@angular/common/http";
 import { Observable } from "rxjs-compat/Observable"
-<<<<<<< HEAD
-import { Login } from "../model/login";
-=======
 import { CookieService } from "ngx-cookie-service";
->>>>>>> nelson_api
 
 @Injectable({
   providedIn: "root"
@@ -13,14 +9,6 @@ import { CookieService } from "ngx-cookie-service";
 export class UsersService {
   constructor(private http: HttpClient, private cookies: CookieService) {}
 
-<<<<<<< HEAD
-  formLogin(login:Login): Observable<any> {
-    return this.http.post("https://reqres.in/api/login", login);
-  }
-}
-
-/* eve.holt@reqres.in mail api */
-=======
   login(user: any): Observable<any> {
   //funciona con  la pagina de ejemplo
   //return this.http.post("https://reqres.in/api/login", user);
@@ -53,4 +41,3 @@ export class UsersService {
   }
 
 }
->>>>>>> nelson_api
