75c906025f9e46cabf40901d081551d742bf4f6a
Nelson Rodriguez <nrodriguez@IPTEL.COM.AR>
2022-11-16 09:39:47 -0300

    pasamos toda la logica a consultas

--- src/app/pages/consultas/consultas.component.ts
+++ src/app/pages/consultas/consultas.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/users/user.service';
+import { MenuService } from 'src/app/menu/menu.service';
 import { Router } from '@angular/router';
+import { CookieService } from "ngx-cookie-service";
+//import { layout1 } from 'src/app/menu/pages/layout1';
 
 @Component({
   selector: 'app-consultas',
@@ -7,10 +11,58 @@ import { Router } from '@angular/router';
   styleUrls: ['./consultas.component.css']
 })
 export class ConsultasComponent implements OnInit {
+  
+  perfil!:string; //perfil login
+  nroCliente!:number; 
+  clienteNro!:number;
+  mensajeCliente!:string;
 
-  constructor() { }
+  listaDatosCliente!:string[];
+  listaSuscripciones!:string[];
+
+  
+
+    constructor(public userService: UsersService, public menuService: MenuService, public router: Router, private cookies: CookieService) {}
 
   ngOnInit(): void {
+   
+    this.perfil=this.userService.getPerfil();
+    console.log(this.perfil);
+    //console.log(this.userService.getNroCliente);
+    //this.nroCliente=nroCliente:this.form.get('nroCliente')?.value
+    this.nroCliente=1000895;
+  
+    console.log(this.clienteNro);
+
+    this.menuService.consultarDatosCliente(this.nroCliente).subscribe( data => {
+      console.log(data);
+      console.log(data.datosCliente.Email);
+      if (data.datosCliente.ActivationCode!=0 ){
+        //if (data.status==200 ){
+        
+        
+        this.listaDatosCliente=[data.datosCliente.ActivationCode
+          ,data.datosCliente.Email
+          ,data.datosCliente.Nombre
+          ,data.datosCliente.Apellido
+          ,data.datosCliente.Estado
+          ,data.datosCliente.Ref
+          ,data.datosCliente.IdRocstar
+          ,data.datosCliente.IdUserRocstar
+          ,data.datosCliente.login
+          ,data.datosCliente.tenantId
+          ,data.datosCliente.modificadoPor];
+        
+          /////this.listaSuscripciones=[data.datosCliente.suscripciones];
+          this.listaSuscripciones=[data.datosCliente.suscripciones[0],data.datosCliente.suscripciones[1]];
+
+    
+      }else {
+        //mostrar por pantalla el mensaje de error
+        this.mensajeCliente="Cliente no encontrado";
+      }
+    });
+
   }
 
 }
